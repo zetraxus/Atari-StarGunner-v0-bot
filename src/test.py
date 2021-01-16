075d93d6f9bc58489e572196c42871b55f73d483
Adam Kapica <adam.kapica@gmail.com>
2021-01-16 14:16:05 +0100

    print iterations in tests

--- src/test.py
+++ src/test.py
@@ -3,14 +3,14 @@ from src.utils import print_logs
 
 def single_test(agent, env, render, save_results, log_filename):
     obs = env.reset()
-    total_reward, done = 0, False
+    total_reward, it, done = 0, 0, False
     while not done:
         env.render() if render else None
         action = agent.get_action(training=False, obs=obs)
         next_obs, reward, done, _ = env.step(action)
-        obs, total_reward = next_obs, total_reward + reward
+        obs, total_reward, it = next_obs, total_reward + reward, it + 1
 
-    print_logs(save_results, log_filename, "Reward (test) = " + str(total_reward))
+    print_logs(save_results, log_filename, "Reward (test) = " + str(total_reward) + ', it = ' + str(it))
     return total_reward
 
 
